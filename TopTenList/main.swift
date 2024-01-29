ec5e668e42b35a886d01589658d73f19196de1d2
Sophie Haber <SHaber@lcs.on.ca>
2024-01-28 22:31:59 -0500

    I added a array

--- TopTenList/main.swift
+++ TopTenList/main.swift
@@ -7,5 +7,23 @@
 
 import Foundation
 
-print("Hello, World!")
 
+//Exercise 1
+
+let iceCreamFlavours = ["Watermelon", "Melon", "Pineapple", "Peach", "Green Apple", "Lemon", "Strawberry", "Mango", "Vamilla", "Coconut"]
+
+print(iceCreamFlavours[0])
+
+print(iceCreamFlavours[4])
+
+print(iceCreamFlavours[9])
+
+//Exercise 2
+
+for i in 0...4 {
+    print (iceCreamFlavours[i])
+}
+
+for iceCream in iceCreamFlavours {
+    print(iceCream)
+}
